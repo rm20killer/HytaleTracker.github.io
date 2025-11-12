7562c74b64b454f9c0ac5736c300cd8e0849dc62
That One Axolotl <thatoneaxolotlsharingthings@gmail.com>
2025-11-12 08:47:59 -0800

    Update news.js

--- news.js
+++ news.js
@@ -404,6 +404,57 @@ function filter(){
         createNewsElements(selectedTags, minDate, maxDate);
     }
 }
+document.getElementById("clear-filters").addEventListener("click", () => {
+    yearToGetJson = currentDate.getFullYear();
+    chunkToGetJson = Number(indexes[String(yearToGetJson)]) + 1 || 0;
+    lastDay = '';
+    const minDate = document.getElementById("min-date-input").value;
+    const maxDate = document.getElementById("max-date-input").value;
+    createNewsElements("null", minDate, maxDate)
+})
+document.getElementById("clear-dates").addEventListener("click", () => {
+    yearToGetJson = currentDate.getFullYear();
+    chunkToGetJson = Number(indexes[String(yearToGetJson)]) + 1 || 0;
+    lastDay = '';
+    yearToGetJson = currentDate.getFullYear();
+    chunkToGetJson = Number(indexes[String(yearToGetJson)]) + 1 || 0;
+    lastDay = '';
+    const form = document.getElementById("filterForm");
+    const children = form.children;
+    let selectedTags = [];
+    console.log(children);
+    for(let child of children){
+        if(child.nodeName == "INPUT"){
+            console.log(child.id);
+            if(child.checked){
+                selectedTags.push(child.id);
+            }
+        }
+    }
+    console.log(selectedTags);
+    const newsItemsToRemove = document.getElementsByClassName("news-item");
+    const dividersToRemove = document.getElementsByClassName("divider");
+    const datesToRemove = document.getElementsByClassName("date");
+    const minDate = document.getElementById("min-date-input").value;
+    const maxDate = document.getElementById("max-date-input").value;
+    while(newsItemsToRemove[0]){
+        newsItemsToRemove[0].remove();
+    }
+    while(dividersToRemove[0]){
+        dividersToRemove[0].remove();
+    }
+    while(datesToRemove[0]){
+        datesToRemove[0].remove();
+    }
+    itemsCreated = 0;
+    if(selectedTags.length == 0){
+        createNewsElements("null", "null", "null");
+    }
+    else{
+        console.log("creating news elements with filter");
+        createNewsElements(selectedTags, "null", "null");
+    }
+})
 
 
 let easterEggKeysPressed = [];
